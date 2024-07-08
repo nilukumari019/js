6b033379c70fc66baf049f1d8ec6d1aa5f6348cf
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-08 08:42:30 +0000

    variables

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";   // treat all js code as newer js version
+// alert(3+3)   we are using nodejs, not browser
+
+let num=2   //number
+console.log(num)
+let name="nilu"
+
+// Number range => 2 to the power 52
+// string =""
+// boolean= true/false
+// null => standlone value
+// undefined
