cab80520a9a3ea4532d4acc07d5ad3948bad28a5
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 10:26:42 +0000

    Number and maths

--- 01_basic/05_numsAndMaths.js
+++ 01_basic/05_numsAndMaths.js
@@ -1,5 +1,32 @@
 // const num=123.8966;
 // console.log(num.toPrecision(1))
 
-const num=123
-console.log(num.toFixed(2));
+// const num=123
+// console.log(num.toFixed(2));
+
+// const num=100000000
+// console.log(num.toLocaleString()) //Internationl system
+// console.log(num.toLocaleString('en-IN')) //Indian system
+
+
+// ***************Maths*************
+
+
+
+// console.log(Math)
+// console.log(Math.round(4.7))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.7))
+
+
+
+
+// console.log(Math.round(Math.random()*10)+1)
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min)+1)+min);
+
+
+
