0920eae7cf4e1de7c47f9d73723475e7291a5185
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 07:17:52 +0000

    Return types of datatype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,17 +1,25 @@
 let score="33";
 
-console.log(score)
-console.log(typeof score)
+// console.log(score)
+// console.log(typeof score)
 let value= Number(score)
-console.log(value);
-console.log(typeof value)
+// console.log(value);
+// console.log(typeof value)
 
 let check=1;
 let bool =Boolean(check);
-console.log(bool);
+// console.log(bool);
 
 
 let num=123
 let string =String(num);
-console.log(string)
-console.log(typeof string)
+// console.log(string)
+// console.log(typeof string)
+
+
+
+// *************Operations***********************
+
+let val=3;
+let negVal=-val
+console.log(negVal)
