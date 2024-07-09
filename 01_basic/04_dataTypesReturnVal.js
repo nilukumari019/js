6b490eed7b945a8d51d085e5e9eacd27562dfd70
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 10:07:06 +0000

    data types

--- 01_basic/04_dataTypesReturnVal.js
+++ 01_basic/04_dataTypesReturnVal.js
@@ -9,8 +9,23 @@
 //        Symbol  =>  symbol
 //        BigInt  =>  bigint
 
+// let a="abcde"
+// console.log(a[3]);
+
+// let s= new String("bcd");
+// console.log(typeof s);
+// console.log(typeof a);
+
+// let str="2+2";
+// let str1= new String("2+3")
+// console.log(eval(str));
+// console.log(eval(str1));
 
 
+let str="mcrifjrifn"
+// console.log(str.at(-3));
+
+console.log("abc".bold())
 
 // 2) Non-primitive Datatypes
 //        Arrays  =>  object
