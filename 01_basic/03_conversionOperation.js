7bc376bd702eab9beaefc2d4b762ee7465e21c87
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 06:07:02 +0000

    conersion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score="33";
+
+console.log(score)
+console.log(typeof score)
+let value= Number(score)
+console.log(value);
+console.log(typeof value)
+
+let check=1;
+let bool =Boolean(check);
+console.log(bool);
+
+
+let num=123
+let string =String(num);
+console.log(string)
+console.log(typeof string)
