05353b6527e389ad39c3abb630a364da8737c6c4
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 12:22:26 +0000

    Date and time

---
+++ 01_basic/06_dateAndTime.js
@@ -0,0 +1,24 @@
+let myDate= new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(typeof myDate);
+
+// let myCreatedDate=new Date(2024, 6,9)
+// let myCreatedDate=new Date(2024, 6,9,14,5,9)
+// console.log(myCreatedDate.toDateString())
+// let myCreatedDate=new Date("2024-07-09")
+// let myCreatedDate=new Date("09-07-2024")
+// console.log(myCreatedDate.toLocaleString())
+
+
+let date=new Date()
+console.log(date.getDate());
+console.log(date.getFullYear());
