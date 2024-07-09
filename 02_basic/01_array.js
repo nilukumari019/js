3d3c01c9b53a8e5b79e9f0a2acb4ff05b678e5ef
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-09 19:41:12 +0000

    Array

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -1,6 +1,19 @@
 let arr1=[1,2,3,4,5]
 let arr2=[0,7,5,9]
-arr1.push(arr2)  // array inside array
-console.log(arr1);
-
+// arr1.push(arr2)  // array inside array
+// console.log(arr1);
 // console.log(arr1[5][5]);
+// let arr=arr1.concat(arr2)
+// console.log(arr)
+
+
+// let arr=[...arr1,...arr2]  // spread operator
+// console.log(arr[7])
+
+
+
+console.log(Array.isArray("Nilu"));
+console.log(Array.from("Nilu"));
+console.log(Array.from({name:"Nilu"}));  // interesting because it's unable to decide on  what basis ....key or value the array should be made
+
+
