baf9bd9e3fb646c445b8d3a958a3da81af4989d0
nilukumari019 <116175257+nilukumari019@users.noreply.github.com>
2024-07-10 14:21:17 +0000

    Object and APIS

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -19,4 +19,24 @@ const obj2={3:"a",4:"b"}
 // const obj3=Object.assign({},obj1,obj2)
 
 const obj3={...obj1,...obj2}
-console.log(obj3)
+// console.log(obj3)
+
+
+const course={
+    coursename:"JS",
+    price:"999",
+    courseInstructor:"Hitesh"
+}
+// const {courseInstructor}=course
+// console.log(courseInstructor)
+
+
+
+//APIS
+{
+    "name":"abc",
+   " price":"9999",
+   "course":"JS"
+}
+
+
